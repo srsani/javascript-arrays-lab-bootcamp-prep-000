9ad61c9d2439ae64a404b7517702d9c757741db0
droll-accelerator-1483 <srsani@gmail.com>
2019-06-20 13:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
+//define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten (name) {
+  kittens[kittens.length] = name
+  return kittens
+}
+
+console.log(destructivelyAppendKitten('Ralph'))
+var kittens = ["Milo", "Otis", "Garfield"];
+
+console.log(kittens)
